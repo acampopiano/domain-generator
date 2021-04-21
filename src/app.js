452e127b32b2bfbf52096b668b3b76b011d09c81
Andres Campopiano <acampopiano@gmail.com>
2021-04-21 14:51:28 +0000

    subo proyecto

--- src/app.js
+++ src/app.js
@@ -2,10 +2,48 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
+import "./assets/img/domaingenerators.png";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big", "breath"];
+  let noun = ["jogger", "racoon", "code"];
+  let dots = [".com", ".net", ".us", ".io", ".de"];
+
+  let domains = [];
+  let domainhacks = [];
+  for (let x = 0; x < pronoun.length; x++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let z = 0; z < noun.length; z++) {
+        for (let i = 0; i < dots.length; i++) {
+          let dn = pronoun[x] + adj[y] + noun[z] + dots[i];
+          domains.push(dn);
+          if (noun[z].substr(-2) === dots[i].substr(-2))
+            domainhacks.push(
+              pronoun[x] + adj[y] + noun[z].substr(0, 2) + dots[i]
+            );
+        }
+      }
+    }
+  }
+
+  var domain = "";
+  domains.forEach(element => {
+    domain += element + "<br/>";
+  });
+  if (domainhacks.length) {
+    domainhacks.forEach(element => {
+      domain +=
+        '<span class="badge badge-pill badge-success">' +
+        element +
+        "</span>" +
+        "<br/>";
+    });
+  }
+  let btn = document.getElementById("generate");
+  btn.addEventListener("click", function() {
+    document.getElementById("domains").innerHTML = domain;
+  });
 };
